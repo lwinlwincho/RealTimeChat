fe823ddd258ff21da7fd823e18b281f5567e4228
lwincho-dev <lwinlwincho135324@gmail.com>
2022-12-28 01:23:13 -0800

    Initial commit

---
+++ app/src/main/java/com/llc/realtimechat/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.llc.realtimechat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.llc.realtimechat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val chatRecyclerViewAdapter = ChatRecyclerViewAdapter()
+
+    private lateinit var auth: FirebaseAuth
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.chatListLiveData.observe(this, Observer{chatList->
+            chatRecyclerViewAdapter.submitList(chatList)
+        })
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.rvChat.apply {
+            adapter = chatRecyclerViewAdapter
+            layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+        }
+
+        binding.btnSend.setOnClickListener {
+            val message = binding.etMessage.text.toString()
+            viewModel.sendMessage(message)
+        }
+    }
+
+    private fun logOut(){
+        auth.signOut()
+        val intent= Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId== R.id.action_logout){
+            logOut()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
